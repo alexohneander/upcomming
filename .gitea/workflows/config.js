b55abe685f8b5ccec32f8d0dc0a3f172cf2b909a
Alex Wellnitz <moin@wellnitz-alex.de>
2025-03-19 08:58:19 +0100

    ci: add renovate pipeline

---
+++ .gitea/workflows/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "endpoint": "https://git.dev-null.rocks/api/v1", // replace it with your actual endpoint
+    "gitAuthor": "Renovate Bot <renovate-bot@dev-null.rocks>",
+    "platform": "gitea",
+    "onboardingConfigFileName": "renovate.json",
+    "autodiscover": true,
+    "optimizeForDisabled": true,
+};
